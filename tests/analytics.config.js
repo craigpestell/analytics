537e7fe7f3acf0bb0540459c09396c5ac4a3ecba
Craig Pestell <m803637@a4370604.federated.fds>
2019-01-18 02:57:36 -0800

    first commit

---
+++ tests/analytics.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    page: {
+      options: {}
+    },
+    elements: [
+      {selector: 'html', 
+       events: {view: {'event_name': 'test'}}},
+      {
+        selector: 'a, img, picture',
+        events: {
+          link: {},
+          impression: {}
+        },
+        data: { generic: (e) => ( new Promise((resolve)=> {
+          resolve({data: 'some generic data'})
+        }))}
+      },
+      /*{
+        selector: '.hl-wrapper',
+        events: {
+          link: {allProducts: 'some data'}
+        },
+        data: {
+          campaign: () => new Promise((resolve) => {resolve({'campaignData': {pull:'from',context: 'attrs'}})}),
+          product: (e) => {
+            console.log('fetch all products here');
+            return new Promise((resolve) => {resolve('something')});
+          }
+        }
+      },*/
+      {
+        selector: '[data-hl-productid]',
+        events: {
+          link: { extra_attr: 'product link event'}
+        },
+        data: {
+          product: (e) => {
+            const prodId = e.currentTarget.dataset['hlProductid'];
+            const url = `https://www.macys.com/xapi/discover/v1/product?productIds=${prodId}&id=3536&_application=SITE&_navigationType=BROWSE&_deviceType=DESKTOP&_shoppingMode=SITE&_regionCode=US&_customerExperiment=271-11,291-21,311-20&currencyCode=USD&_customerState=GUEST&clientId=QV`
+            // return new Promise((resolve) => {resolve(fetch(url))}); 
+            return new Promise((resolve) => {resolve({product_id: prodId})}); 
+          }
+        }
+      }
+    ]
+  };
--- tests/analytics.spec.js
+++ tests/analytics.spec.js
@@ -1,3 +1,7 @@
+import { AnalyticsBootstrap } from '../src/analytics';
+
+import analyticsConfig from './analytics.config';
+
 describe('analytics', () => {
   let data;
 
@@ -7,8 +11,53 @@ describe('analytics', () => {
   });
 
   it('should have tests', () => {
+    const AB = AnalyticsBootstrap(analyticsConfig);
+
     expect(data).toEqual({ foo: 'bar', abc: 'xyz' });
   });
+
+  it('should be a function', () => {
+    expect(typeof AnalyticsBootstrap).toBe("function");
+  });
+
+  it('should return a module when invoked', () => {
+    expect(typeof AnalyticsBootstrap()).toEqual("object");
+  });
+
+  it('should have bootstrapAnalyticsTagging method', () => {
+    expect (typeof AnalyticsBootstrap().bootstrapAnalyticsTagging).toEqual("function");
+  });
+
+  it('should add event listeners..', () => {
+    const AB = AnalyticsBootstrap(analyticsConfig);
+
+    function getAnalyticsData(config) {
+    
+      if( typeof config === "function") {
+        return config;
+      }
+  
+      if(config.product){
+        if( typeof config.product === "function") {
+          return config.product;
+        }
+    
+        /*const { product } = config.data;
+  
+  
+        const data = Promise.all(
+          product.map(async(i) => await( await fetch(`https://www.macys.com/xapi/discover/v1/product?productIds=${i}&_application=SITE&_deviceType=DESKTOP&_shoppingMode=SITE&_regionCode=US&currencyCode=USD&_customerState=GUEST&clientId=QV`)).json())
+        );
+        return data;*/
+      }
+  
+      return new Promise(() => {return null});
+    }
+  
+    AB.bootstrapAnalyticsTagging(getAnalyticsData);
+  
+  })
+
 
   it('might have async tests', (done) => {
     Promise.resolve('foobar').then((result) => {
@@ -16,4 +65,27 @@ describe('analytics', () => {
       done();
     });
   });
-});
+})
+
+describe('bootstrapAnalytics', () => {
+  let AB;
+  beforeEach(() => {
+    AB = undefined;
+  });
+
+  it('should fire page view tag', (done) => {
+    
+    AB = AnalyticsBootstrap(analyticsConfig);
+    
+    spyOn(AB, 'fireTag');
+    
+    AB.bootstrapAnalyticsTagging();
+
+    setTimeout(() => {
+      expect(AB.fireTag).toHaveBeenCalledWith(['view', {}])
+      done();
+    }, 1000)
+    
+  });
+
+})

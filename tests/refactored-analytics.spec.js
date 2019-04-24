d29a6bee9f7a2c2873390c1eaa14a0968fe013cc
Craig Pestell <craig.pestell@macys.com>
2019-04-24 06:00:52 -0700

    E-10240: initial scaffold commit

---
+++ tests/refactored-analytics.spec.js
@@ -0,0 +1,19 @@
+describe('refactored-analytics', () => {
+  let data;
+
+  beforeEach(() => {
+    loadFixtures('main.html'); // fixtures/html/main.html becomes your DOM
+    data = getJSONFixture('refactored-analytics.json');
+  });
+
+  it('should have tests', () => {
+    expect(data).toEqual({ foo: 'bar', abc: 'xyz' });
+  });
+
+  it('might have async tests', (done) => {
+    Promise.resolve('foobar').then((result) => {
+      expect(result).toBe('foobar');
+      done();
+    });
+  });
+});

c8fe3039399cc426238e1d238c9267bd3983f786
Craig Pestell <craig.pestell@macys.com>
2019-05-29 12:24:47 -0700

    unit test coverage at 80%

--- tests/analytics/src/util/constants.spec.js
+++ tests/analytics/src/util/constants.spec.js
@@ -4,6 +4,6 @@ import * as constants from '../../../../src/js/util/constants';
 console.log({constants})
 describe('event type constant', () => {
     it('should have a proper string coercing method (Symbol.toPrimitive}', () =>{
-        expect(constants.EVENT_TYPE.link).toEqual('link');
+        expect(`$(constants.EVENT_TYPE.link)`).toEqual('link');
     });
 })

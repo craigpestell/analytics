98f54d798deb956fc43d4e825a253a236abbcc1a
Craig Pestell <craigpestell@gmail.com>
2023-12-27 06:42:30 -0800

    0.0.87

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import Analytics, { PageViewProps } from './lib/analytics';
-import { UsePageViewProps } from './hooks/useAnalytics';
 
-export { Analytics, type PageViewProps, type UsePageViewProps };
+export { Analytics, type PageViewProps };
 
 export default Analytics;
